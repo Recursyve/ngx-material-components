892131430b6cabec68afee6981867f09d4a3df5a
JulienDuf <julien@recursyve.io>
2024-11-26 17:00:31 -0500

    Initial setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-root",
+    standalone: true,
+    imports: [],
+    templateUrl: "./app.template.html",
+    styleUrl: "./app.style.scss"
+})
+export class AppComponent {}
