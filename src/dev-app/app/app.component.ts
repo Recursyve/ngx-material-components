f9398082f080501b0f0905867cfc6401cc7a2fa3
JulienDuf <julien@recursyve.io>
2025-02-08 21:34:52 -0500

    feat: Initial typeahead implementation based on the mat-select codebase

--- src/dev-app/app/app.component.ts
+++ src/dev-app/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from "@angular/core";
+import { JsonPipe } from "@angular/common";
+import { Component, inject, signal } from "@angular/core";
+import { FormBuilder, FormControl, ReactiveFormsModule } from "@angular/forms";
+import { MatButton } from "@angular/material/button";
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatOption, MatSelect } from "@angular/material/select";
+import { NiceTypeaheadComponent } from "@recursyve/ngx-material-components/typeahead";
 
 @Component({
     selector: "nice-root",
     standalone: true,
-    imports: [],
+    imports: [
+        MatLabel,
+        MatFormField,
+        NiceTypeaheadComponent,
+        MatSelect,
+        MatOption,
+        MatButton,
+        ReactiveFormsModule,
+        JsonPipe
+    ],
     templateUrl: "./app.template.html",
     styleUrl: "./app.style.scss"
 })
-export class AppComponent {}
+export class AppComponent {
+    private _fb = inject(FormBuilder);
+
+    public items = ["Apple", "Banana", "Orange", "Pear", "Strawberry"];
+    public formGroup = this._fb.group({
+        typeahead: this._fb.control(""),
+        asyncTypeahead: this._fb.control(""),
+    });
+
+    public typeaheadValue = signal({});
+
+    public displayResult(): void {
+        this.typeaheadValue.set(this.formGroup.getRawValue());
+    }
+}
---
+++ src/material-components/typeahead/typeahead.component.ts
@@ -0,0 +1,78 @@
+import { CdkConnectedOverlay, CdkOverlayOrigin } from "@angular/cdk/overlay";
+import { ChangeDetectionStrategy, Component, computed, input, ViewEncapsulation } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatOption } from "@angular/material/core";
+import { MatFormField, MatFormFieldControl } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { matSelectAnimations } from "@angular/material/select";
+import { NiceTypeaheadBase } from "./typeahead-base.component";
+
+export type SearchFunction<T> = (search: string, item: T) => boolean;
+
+@Component({
+    selector: "nice-typeahead",
+    standalone: true,
+    imports: [
+        CdkOverlayOrigin,
+        CdkConnectedOverlay,
+        ReactiveFormsModule,
+        MatOption,
+        MatFormField,
+        MatInput
+    ],
+    templateUrl: "./typeahead.template.html",
+    styleUrl: "./typeahead.style.scss",
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
+    animations: [matSelectAnimations.transformPanel],
+    providers: [{ provide: MatFormFieldControl, useExisting: NiceTypeaheadComponent }],
+    host: {
+        "role": "combobox",
+        "aria-haspopup": "listbox",
+        "class": "nice-typeahead",
+        "[attr.id]": "id",
+        "[attr.aria-controls]": "panelOpen ? id + \"-panel\" : null",
+        "[attr.aria-expanded]": "panelOpen",
+        "[attr.aria-required]": "required.toString()",
+        "[attr.aria-disabled]": "disabled.toString()",
+        "[attr.aria-invalid]": "errorState",
+        "[class.nice-typeahead-disabled]": "disabled",
+        "[class.nice-typeahead-invalid]": "errorState",
+        "[class.nice-typeahead-required]": "required",
+        "[class.nice-typeahead-empty]": "empty",
+        "(keydown)": "_handleKeydown($event)",
+        "(focus)": "onFocusChanged(true)",
+        "(blur)": "onFocusChanged(false)"
+    }
+})
+export class NiceTypeaheadComponent<T> extends NiceTypeaheadBase<T> {
+    public readonly values = input.required<T[]>();
+    public readonly searchFn = input<SearchFunction<T>>();
+
+    public readonly filteredValues = computed(() => this.filterValuesFromSearch(this._searchValue(), this.values()));
+
+    public override onFocusChanged(isFocused: boolean): void {
+        super.onFocusChanged(isFocused);
+
+        if (isFocused) {
+            this._searchControl.patchValue("");
+        }
+    }
+
+    public filterValuesFromSearch(searchValue: string, values: T[]): T[] {
+        const _searchValue = searchValue.toLowerCase();
+        const fn = this.searchFn();
+        if (fn) {
+            return values.filter((v) => fn(_searchValue, v));
+        }
+
+        return values.filter((v) => {
+            if (typeof v === "string") {
+                return v.toLowerCase().includes(_searchValue);
+            }
+
+            // TODO: Check for properties in object?
+            return false;
+        });
+    }
+}
