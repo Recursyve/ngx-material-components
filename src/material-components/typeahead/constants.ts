eb5feeb6ca5fdf6a7fc270912212df207f85db59
Gabriel Roberge <groberge@recursyve.io>
2026-07-02 23:18:29 -0400

    feat(typeahead): add config and provideTypeahead for icon customization
    Introduce NiceTypeaheadConfig with arrowIcon, removeIcon and searchIcon
    options, backed by NICE_TYPEAHEAD_CONFIG and provideTypeahead().

--- src/material-components/typeahead/constants.ts
+++ src/material-components/typeahead/constants.ts
@@ -1,3 +1,6 @@
 import { InjectionToken } from "@angular/core";
+import { NiceTypeaheadConfig } from "./config";
 
 export const NICE_ASYNC_TYPEAHEAD_RESOURCES_PROVIDER = new InjectionToken("nice_async_typeahead_provider");
+
+export const NICE_TYPEAHEAD_CONFIG = new InjectionToken<NiceTypeaheadConfig>("nice_typeahead_config");
